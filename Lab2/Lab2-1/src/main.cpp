fff6fc830efeccf51c019bcab2c13f56a38eea40
pphisit <109591322+pphisit@users.noreply.github.com>
2023-09-15 00:36:42 +0700

    2

--- Lab2/Lab2-1/src/main.cpp
+++ Lab2/Lab2-1/src/main.cpp
@@ -1,5 +1,5 @@
 //1.กดปุ่มกดดับปล่อยดิด LED แบบ Pull-Down โดยใช้ Interrupt และ Debounce
-
+/*
 #include <Arduino.h>
 
 const int LED_ON = 0;  
@@ -39,3 +39,66 @@ void loop() {
       break;
   }
 }
+*/
+#include <Arduino.h>
+
+// กำหนดขา GPIO ที่เชื่อมกับ LED
+const int ledPin = D2;
+
+// กำหนดขา GPIO ที่เชื่อมกับปุ่ม
+const int buttonPin = D1;
+
+// กำหนดตัวแปรสำหรับ State Machine
+enum State { LED_OFF, LED_ON, WAIT_FOR_RELEASE };
+State currentState = LED_ON;
+
+// กำหนดตัวแปรสำหรับเก็บค่าปุ่มขณะกดและปล่อย
+volatile bool buttonPressed = false;
+
+IRAM_ATTR void handleButtonPress() {
+  // ในฟังชั่นนี้จะถูกเรียกเมื่อเกิดการกดปุ่ม
+  if (!buttonPressed) {
+    // ตรวจสอบ Debounce โดยรอเวลา debounceDelay
+    delay(50);
+    if (digitalRead(buttonPin) == HIGH) {
+      buttonPressed = true;
+    }
+  }
+}
+void setup() {
+  // เริ่มต้น Serial Monitor
+  Serial.begin(115200);
+
+  // กำหนดขาที่เชื่อมกับ LED เป็น OUTPUT
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  // กำหนดขาที่เชื่อมกับปุ่มเป็น INPUT_PULLUP
+  pinMode(buttonPin, INPUT);
+
+
+  // กำหนดให้ฟังชั่น handleButtonPress เรียกใช้งานเมื่อเกิดการกดปุ่ม
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonPress, RISING);
+}
+
+void loop() {
+  switch (currentState) {
+    case LED_OFF:
+      digitalWrite(ledPin, LOW);
+      if (!buttonPressed) {
+        currentState = LED_ON;
+      }
+      break;
+
+
+
+    case LED_ON:
+      digitalWrite(ledPin, HIGH);
+      if (buttonPressed) {
+        currentState = LED_OFF;
+      }
+      break;
+  }
+}
+
+
--- Lab3/Lab3-4/src/main.cpp
+++ Lab3/Lab3-4/src/main.cpp
@@ -1,57 +1,47 @@
-//ผ่าน
+//4.ใช้ตัวต้านทานปรับค่าได้ เพิ่มลดความเร็วมอเตอร์และใช้ปุ่ม เพื่อเปลี่ยนการหมุนมอเตอร์เป็นทวนเข็มนาฬิกาและตามเข็มนาฬิกาผ่าน
+//อยากทดสอบใหม่
+
 #include <Arduino.h>
 
-// กำหนดขา GPIO ที่เชื่อมกับมอเตอร์
-const int motorPin1 = D5; // สำหรับควบคุมทิศทางการหมุน
-const int motorPin2 = D6; // สำหรับควบคุมทิศทางการหมุน
-//const int pwmPin = 14;   // สำหรับควบคุมความเร็ว PWM
-int motorSpeed ;
-// กำหนดขา GPIO ที่เชื่อมกับปุ่ม
 const int buttonPin = D1;
+const int motorPin1 = D5; 
+const int motorPin2 = D6; 
+int motorSpeed ;
+int state;
+const int CLOCKWISE = 0;
+const int ANTICLOCKWISE = 1;
 
-// กำหนดตัวแปรสำหรับ State Machine
-enum State { STOPPED, clockwise, anticlockwise };
-State currentState = clockwise; 
 void setup() {
-  // เริ่มต้น Serial Monitor
-  Serial.begin(115200);
-
-  // กำหนดขาที่เชื่อมกับมอเตอร์เป็น OUTPUT
+  Serial.begin(115200); 
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-
-  // กำหนดขา PWM เป็น OUTPUT
-  //pinMode(pwmPin, OUTPUT);
-
-  // กำหนดขาที่เชื่อมกับปุ่มเป็น INPUT_PULLUP
   pinMode(buttonPin, INPUT);
-
+  state = CLOCKWISE;
 }
 
 void loop() {
 
-  switch (currentState) {
+  switch (state) {
 
-    case clockwise:
+    case CLOCKWISE:
       motorSpeed =map(analogRead(A0),0,1023,0,255);
       Serial.println("clockwise  "+ String(motorSpeed));
-      //analogWrite(, 255); // เพิ่มความเร็วของ PWM
-      analogWrite(motorPin1, motorSpeed); // ทิศทางการหมุนข้างหน้า
+      analogWrite(motorPin1, motorSpeed); 
       analogWrite(motorPin2, LOW);
-      if (digitalRead(buttonPin) == HIGH) {
-        currentState = anticlockwise; // ถ้าปุ่มถูกกด สั่งให้มอเตอร์หมุนข้างหลัง
-      }
+      if (digitalRead(buttonPin) == HIGH){
+        state = ANTICLOCKWISE; 
+      } 
       break;
 
-    case anticlockwise:
-      //analogWrite(pwmPin, 255); // เพิ่มความเร็วของ PWM
+    case ANTICLOCKWISE:     
       motorSpeed =map(analogRead(A0),0,1023,0,255);
       Serial.println("anticlockwise  "+ String(motorSpeed));
-      analogWrite(motorPin1, LOW); // ทิศทางการหมุนข้างหลัง
+      analogWrite(motorPin1, LOW); 
       analogWrite(motorPin2, motorSpeed);
-      if (digitalRead(buttonPin) == HIGH) {
-        currentState = clockwise; // ถ้าปุ่มถูกกด สั่งให้มอเตอร์หยุดหมุน
-      }
+      if (digitalRead(buttonPin) == HIGH){
+        state = CLOCKWISE; 
+      } 
       break;
+      }      
   }
-}
+
