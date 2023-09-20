152b779eb694983bb241bbe4ae0457943f1e4e3f
Phisit Kongkaew <109591322+pphisit@users.noreply.github.com>
2023-09-20 16:25:11 +0700

    gkuil

--- Lab4(ปรับปรุง)/Lab4-3/src/main.cpp
+++ Lab4(ปรับปรุง)/Lab4-3/src/main.cpp
@@ -1,10 +1,11 @@
 // 3.แสดงตัวเลข 1234 ผ่าน 7-segment ด้วย MCP23008
 
+
 #include <Arduino.h>
 #include <Adafruit_MCP23X08.h> // ประกาศใช้งานไลบารี่ Adafruit_MCP23X08
 Adafruit_MCP23X08 mcp;         // ประกาศตัวแปร mcp ให้เป็นตัวแปรของคลาส Adafruit_MCP23X08
-int Digit_pins[] = {0, 1, 2, 3};               // กำหนด Port สำหรับขา Digit 4 ถึง 1
-int Segment_pin[] = {D0, D3, D4, D5, D6, D7, D8}; // กำหนด Port สำหรับขา Segment  A ถึง G
+int Digit_pins[] = {D5, D6, D7, D8};               // กำหนด Port สำหรับขา Digit 4 ถึง 1
+int Segment_pin[] = {0,1,2,3,4,5,6}; // กำหนด Port สำหรับขา Segment  A ถึง G
 const int SHOW_DIGIT_1 = 1;
 const int SHOW_DIGIT_2 = 2;
 const int SHOW_DIGIT_3 = 3;
@@ -16,91 +17,100 @@ void setup()
     mcp.begin_I2C(); 
     state = SHOW_DIGIT_1;
     // กำหนดให้ขาที่ใช้เป็น Output ประกาศทีล่ะ Port จากตัวแปร Digit_pins
-    
-        mcp.pinMode(Digit_pins[0], OUTPUT);
-        mcp.pinMode(Digit_pins[1], OUTPUT);
-        mcp.pinMode(Digit_pins[2], OUTPUT);
-        mcp.pinMode(Digit_pins[3], OUTPUT);
+    for (int i = 0; i < 4; i++)
+    {
+        pinMode(Digit_pins[i], OUTPUT);
+    }
+       
     // กำหนดให้ขาที่ใช้เป็น Output ประกาศทีล่ะ Port จากตัวแปร Segment_pin
+    
     for (int i = 0; i < 7; i++)
     {
-        pinMode(Segment_pin[i], OUTPUT);
+        mcp.pinMode(Segment_pin[i], OUTPUT);
     }
     
 
 }
 void loop()
 {
-                  if (state == SHOW_DIGIT_1)
+    switch (state)
     {
+    case SHOW_DIGIT_1:
+        /* code */
+    
+    
          // กำหนดให้ขาเปิดใช้ขา Digit 1 ทำงาน และปิดขา Digit 2,3,4
-        mcp.digitalWrite(Digit_pins[0], HIGH);
-        mcp.digitalWrite(Digit_pins[1], HIGH);
-        mcp.digitalWrite(Digit_pins[2], HIGH);
-        mcp.digitalWrite(Digit_pins[3], LOW);
+        digitalWrite(Digit_pins[0], HIGH);
+        digitalWrite(Digit_pins[1], HIGH);
+        digitalWrite(Digit_pins[2], HIGH);
+        digitalWrite(Digit_pins[3], LOW);
         // แสดงเลข 4 บน 7 Segment
-        digitalWrite(Segment_pin[0], LOW);
-        digitalWrite(Segment_pin[1], HIGH);
-        digitalWrite(Segment_pin[2], HIGH);
-        digitalWrite(Segment_pin[3], LOW);
-        digitalWrite(Segment_pin[4], LOW);
-        digitalWrite(Segment_pin[5], HIGH);
-        digitalWrite(Segment_pin[6], HIGH);
-        delay(1);
-        state = SHOW_DIGIT_2;
-    }
-    else if (state == SHOW_DIGIT_2)
-    {
+        mcp.digitalWrite(Segment_pin[0], LOW);
+        mcp.digitalWrite(Segment_pin[1], HIGH);
+        mcp.digitalWrite(Segment_pin[2], HIGH);
+        mcp.digitalWrite(Segment_pin[3], LOW);
+        mcp.digitalWrite(Segment_pin[4], LOW);
+        mcp.digitalWrite(Segment_pin[5], HIGH);
+        mcp.digitalWrite(Segment_pin[6], HIGH);
+        delay(500);
+        state = SHOW_DIGIT_2;        
+        break;
+
+    
+   case SHOW_DIGIT_2:
+    
         // กำหนดให้ขาเปิดใช้ขา Digit 2 ทำงาน และปิดขา Digit 1,3,4
-        mcp.digitalWrite(Digit_pins[0], HIGH);
-        mcp.digitalWrite(Digit_pins[1], HIGH);
-        mcp.digitalWrite(Digit_pins[2], LOW);
-        mcp.digitalWrite(Digit_pins[3], HIGH);
+        digitalWrite(Digit_pins[0], HIGH);
+        digitalWrite(Digit_pins[1], HIGH);
+        digitalWrite(Digit_pins[2], LOW);
+        digitalWrite(Digit_pins[3], HIGH);
         // แสดงเลข 3 บน 7 Segment
-        digitalWrite(Segment_pin[0], HIGH);
-        digitalWrite(Segment_pin[1], HIGH);
-        digitalWrite(Segment_pin[2], HIGH);
-        digitalWrite(Segment_pin[3], HIGH);
-        digitalWrite(Segment_pin[4], LOW);
-        digitalWrite(Segment_pin[5], LOW);
-        digitalWrite(Segment_pin[6], HIGH);
-        delay(1);
+        mcp.digitalWrite(Segment_pin[0], HIGH);
+        mcp.digitalWrite(Segment_pin[1], HIGH);
+        mcp.digitalWrite(Segment_pin[2], HIGH);
+        mcp.digitalWrite(Segment_pin[3], HIGH);
+        mcp.digitalWrite(Segment_pin[4], LOW);
+        mcp.digitalWrite(Segment_pin[5], LOW);
+        mcp.digitalWrite(Segment_pin[6], HIGH);
+        delay(500);
         state = SHOW_DIGIT_3;
-    }
-    else if (state == SHOW_DIGIT_3)
-    {
+        break;
+    
+    case SHOW_DIGIT_3:
+    
         // กำหนดให้ขาเปิดใช้ขา Digit 3 ทำงาน และปิดขา Digit 1,2,4
-        mcp.digitalWrite(Digit_pins[0], HIGH);
-        mcp.digitalWrite(Digit_pins[1], LOW);
-        mcp.digitalWrite(Digit_pins[2], HIGH);
-        mcp.digitalWrite(Digit_pins[3], HIGH);
+        digitalWrite(Digit_pins[0], HIGH);
+        digitalWrite(Digit_pins[1], LOW);
+        digitalWrite(Digit_pins[2], HIGH);
+        digitalWrite(Digit_pins[3], HIGH);
         // แสดงเลข 2 บน 7 Segment
-        digitalWrite(Segment_pin[0], HIGH);
-        digitalWrite(Segment_pin[1], HIGH);
-        digitalWrite(Segment_pin[2], LOW);
-        digitalWrite(Segment_pin[3], HIGH);
-        digitalWrite(Segment_pin[4], HIGH);
-        digitalWrite(Segment_pin[5], LOW);
-        digitalWrite(Segment_pin[6], HIGH);
-        delay(1);
+        mcp.digitalWrite(Segment_pin[0], HIGH);
+        mcp.digitalWrite(Segment_pin[1], HIGH);
+        mcp.digitalWrite(Segment_pin[2], LOW);
+        mcp.digitalWrite(Segment_pin[3], HIGH);
+        mcp.digitalWrite(Segment_pin[4], HIGH);
+        mcp.digitalWrite(Segment_pin[5], LOW);
+        mcp.digitalWrite(Segment_pin[6], HIGH);
+        delay(500);
         state = SHOW_DIGIT_4;
-    }
-    else if (state == SHOW_DIGIT_4)
-    {
+    break;
+   case SHOW_DIGIT_4:
+    
          // กำหนดให้ขาเปิดใช้ขา Digit 4 ทำงาน และปิดขา Digit 1,2,3
-        mcp.digitalWrite(Digit_pins[0], LOW);
-        mcp.digitalWrite(Digit_pins[1], HIGH);
-        mcp.digitalWrite(Digit_pins[2], HIGH);
-        mcp.digitalWrite(Digit_pins[3], HIGH);
+        digitalWrite(Digit_pins[0], LOW);
+        digitalWrite(Digit_pins[1], HIGH);
+        digitalWrite(Digit_pins[2], HIGH);
+        digitalWrite(Digit_pins[3], HIGH);
         // แสดงเลข 1 บน 7 Segment
-        digitalWrite(Segment_pin[0], LOW);
-        digitalWrite(Segment_pin[1], HIGH);
-        digitalWrite(Segment_pin[2], HIGH);
-        digitalWrite(Segment_pin[3], LOW);
-        digitalWrite(Segment_pin[4], LOW);
-        digitalWrite(Segment_pin[5], LOW);
-        digitalWrite(Segment_pin[6], LOW);
-        delay(1);
+        mcp.digitalWrite(Segment_pin[0], LOW);
+        mcp.digitalWrite(Segment_pin[1], HIGH);
+        mcp.digitalWrite(Segment_pin[2], HIGH);
+        mcp.digitalWrite(Segment_pin[3], LOW);
+        mcp.digitalWrite(Segment_pin[4], LOW);
+        mcp.digitalWrite(Segment_pin[5], LOW);
+        mcp.digitalWrite(Segment_pin[6], LOW);
+        delay(500);
         state = SHOW_DIGIT_1;
+        break;
     }
 }
--- Lab4(ปรับปรุง)/Lab4-4/src/main.cpp
+++ Lab4(ปรับปรุง)/Lab4-4/src/main.cpp
@@ -1,10 +1,11 @@
 //•	โจทย์ประยุกต์ แจ้งเตือนอุณหภูมิผ่าน LED 3 ดวง เมื่อถึงค่าอุณหภูมิที่กำหนด
 #include <arduino.h>
-#include <Wire.h>                  
+#include <Wire.h>
 #include "SHTC3.h"
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
-SHTC3 shtc3(Wire); 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+SHTC3 shtc3(Wire);
+
 // กำหนดขา GPIO ที่เชื่อมกับ LED
 const int ledPin1 = D3;
 const int ledPin2 = D4;
@@ -12,15 +13,15 @@ const int ledPin3 = D5;
 
 // กำหนดค่าอุณหภูมิที่ต้องการสำหรับการเปิด LED
 const float tempThreshold1 = 25.0;
-const float tempThreshold2 = 27.0;
-const float tempThreshold3 = 30.0;
+const float tempThreshold2 = 26.0;
+const float tempThreshold3 = 27.0;
+
+float currentTemperature;
+float temperature;
 
 // กำหนดตัวแปรสำหรับ State Machine
-enum State { MEASURE_TEMPERATURE, CHECK_THRESHOLD };
+enum State { MEASURE_TEMPERATURE, CHECK_THRESHOLD, ALERT };
 State currentState = MEASURE_TEMPERATURE;
-
-// สร้างอินสแตนซ์ของ SHTC3
-
 
 void setup() {
   // เริ่มต้น Serial Monitor
@@ -30,13 +31,10 @@ void setup() {
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
-  
-  // กำหนดขนาดของจอ LCD (จอ 16x2)
-  lcd.begin(16, 2);
 
   // เริ่มต้นเซนเซอร์ SHTC3
   Wire.begin();
-  lcd.begin();
+  lcd.init();
   lcd.backlight();
   // รอสักครู่ให้เซนเซอร์เตรียมตัว
   //delay(500);
@@ -46,177 +44,50 @@ void loop() {
   switch (currentState) {
     case MEASURE_TEMPERATURE:
       // อ่านค่าอุณหภูมิจากเซนเซอร์ SHTC3
-      float temperature = shtc3.readTemperature();
+      shtc3.begin();
+      shtc3.sample();
+      temperature = shtc3.readTempC();
+      
 
       // แสดงค่าอุณหภูมิบนจอ LCD
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Temp: ");
-      lcd.setCursor(7, 1);
+      lcd.setCursor(6, 0);
       lcd.print(temperature, 1);
-      lcd.setCursor(9, 1);
+      lcd.setCursor(13, 0);
       lcd.print("C");
-
+      Serial.print("Temperature: ");
+      Serial.println(temperature);
+        delay(1000);
       // เปลี่ยนสถานะเพื่อตรวจสอบค่าอุณหภูมิ
       currentState = CHECK_THRESHOLD;
       break;
-
     case CHECK_THRESHOLD:
       // อ่านค่าอุณหภูมิ
-      float currentTemperature = shtc3.readTemperature();
+      currentTemperature = shtc3.readTempC();
 
       // ตรวจสอบและควบคุม LED ตามค่าอุณหภูมิ
       if (currentTemperature >= tempThreshold1) {
         digitalWrite(ledPin1, HIGH);
       } else {
         digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin1, LOW); 
-        digitalWrite(ledPin1, LOW);         
       }
 
       if (currentTemperature >= tempThreshold2) {
-        digitalWrite(ledPin1, HIGH);
         digitalWrite(ledPin2, HIGH);
       } else {
-        digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin2, LOW); 
-        digitalWrite(ledPin3, LOW); 
+        digitalWrite(ledPin2, LOW);
       }
 
       if (currentTemperature >= tempThreshold3) {
-        digitalWrite(ledPin1, HIGH);
-        digitalWrite(ledPin2, HIGH);
         digitalWrite(ledPin3, HIGH);
       } else {
-        digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin2, LOW); 
-        digitalWrite(ledPin3, LOW); 
+        digitalWrite(ledPin3, LOW);
       }
+
       // กลับไปวัดอุณหภูมิอีกครั้ง
       currentState = MEASURE_TEMPERATURE;
       break;
   }
 }
-
-/*
-//4.กำหนดใช้ LED 4 ดวง มีปุ่มกด 2 ปุ่ม โดยปุ่มที่ 1 สามารถควบคุม LED ขยับไปทางขวา ส่วนปุ่มที่ 2 สามารถควบคุมขยับไปทางซ้าย เมื่อกด 1 ครั้ง โดย LED เริ่มต้นจะอยู่ซ้ายมือสุด     
-#include <Arduino.h>
-
-// กำหนดขาของ LED
-const int ledPins[] = {D1, D2};
-//const int numLEDs = sizeof(ledPins) / sizeof(ledPins[0]);
-
-// กำหนดขาของปุ่ม
-const int buttonChangeStatePin = D7;
-const int buttonReduceLEDPin = D8;
-
-// กำหนดสถานะ
-enum State {
-  STATE_1,
-  STATE_2,
-  STATE_3,
-  STATE_4
-};
-
-
-
-State currentState = STATE_1;
-//int numActiveLEDs = 1;
-/*
-void changeState() {
-  // เปลี่ยนสถานะแต่ละครั้งที่ปุ่มถูกกด
-  switch (currentState) {
-    case STATE_1:
-      digitalWrite(ledPins[0], HIGH);
-      digitalWrite(ledPins[1], LOW);      
-      currentState = STATE_2;      
-      break;
-    case STATE_2:
-      digitalWrite(ledPins[0], LOW);
-      digitalWrite(ledPins[1], HIGH);
-      currentState = STATE_3;
-      break;
-    case STATE_3:
-      currentState = STATE_4;
-      break;
-    case STATE_4:
-      currentState = STATE_1;
-      break;
-  }
-}
-
-void reduceLEDCount() {
-  // ลดจำนวน LED ที่ติดตาม State Machine
-  if (numActiveLEDs > 1) {
-    numActiveLEDs--;
-  }
-}
-
-void updateLEDs() {
-  // ปิด LED ทุกดวง
-  for (int i = 0; i < numLEDs; i++) {
-    digitalWrite(ledPins[i], LOW);
-  }
-
-  // เปิด LED ตามจำนวนที่ต้องการ
-  for (int i = 0; i < numActiveLEDs; i++) {
-    digitalWrite(ledPins[i], HIGH);
-  }
-}
-*/
-/*
-void setup() {
-  // กำหนดขา LED เป็น OUTPUT
- // for (int i = 0; i < numLEDs; i++) {
- //   pinMode(ledPins[i], OUTPUT);
- //   digitalWrite(ledPins[i], LOW);  // ปิด LED ทุกดวงเริ่มต้น
- // }
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  // กำหนดขาปุ่มเป็น INPUT_PULLUP
-  pinMode(buttonChangeStatePin, INPUT);
-  pinMode(buttonReduceLEDPin, INPUT);
-
-  Serial.begin(115200);
-}
-
-void loop() {
-  // อ่านสถานะปุ่มเพื่อเปลี่ยนสถานะ
-  if (digitalRead(buttonChangeStatePin) == LOW) {
-    Serial.println("buttonChangeStatePin");
-    currentState = static_cast<State>((currentState + 1));
-    //changeState();
-    delay(100);  // ป้องกันการกระโดดสถานะ
-  }
-
-  // อ่านสถานะปุ่มเพื่อลดจำนวน LED
-  if (digitalRead(buttonReduceLEDPin) == HIGH) {
-    currentState = static_cast<State>((currentState - 1));
-   // reduceLEDCount();
-    delay(100);  // ป้องกันการกระโดดสถานะ
-  }
-
- switch (currentState) {
-    case STATE_1:
-      digitalWrite(ledPins[0], HIGH);
-      digitalWrite(ledPins[1], LOW);   
-      Serial.println("STATE_1");   
-      currentState = STATE_2;      
-      break;
-    case STATE_2:
-      digitalWrite(ledPins[0], LOW);
-      digitalWrite(ledPins[1], HIGH);
-      Serial.println("STATE_2");
-      currentState = STATE_3;
-      break;
-    case STATE_3:
-      currentState = STATE_4;
-      break;
-    case STATE_4:
-      currentState = STATE_1;
-      break;
-  }
-}
-  // อัพเดทสถานะ LED ตาม State Machine
-
-*/
